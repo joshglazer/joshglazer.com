f30ddb557610f85bab226035b3c00660dc7c3263
Josh Glazer <joshglazer@gmail.com>
2021-03-11 20:21:53 -0500

    filled in the blanks with content relevant to me

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,56 +2,63 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Josh Glazer | Front-End Architect | Full Stack Developer | Really Smart Person', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my portfolio.  Check out some cool projects I have built.', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hi! My name is',
+  name: 'Josh Glazer',
+  subtitle: "I'm a Full Stack Developer, and really enjoy working with Front-End code.",
+  cta: 'Get To Know Me!',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'joshglazer-headshot.png',
+  paragraphOne:
+    "Hi! I'm Josh, it's nice to meet you.  I am a software engineer from Baltimore, MD.",
+  paragraphTwo:
+    "I really like to code, and have been doing it for a very long time.  When I'm not writing code, you might find me playing piano, watching the Orioles or Ravens, or eating bagels.",
+  paragraphThree: 'Check out some of my projects below.',
+  resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'SpotYou.png',
+    title: 'SpotYou',
+    info:
+      'SpotYou is a React/Gatsby web application that lets you connect Spotify and Youtube to watch your favorite music videos.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://spotyou.joshglazer.com/',
+    repo: 'https://github.com/joshglazer/SpotYou',
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'spower-hour.png',
+    title: 'Spower Hour',
+    info:
+      'Spower Hour is an Angular 11 web application that lets you listen to the first minute of every song in a Spotify playlist.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://spowerhour.joshglazer.com/now-playing',
+    repo: 'https://github.com/joshglazer/spower-hour',
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'reading-hero.png',
+    title: 'Reading Hero',
+    info:
+      'Reading Hero is a mobile application built using Angular and the Ionic framework.  It is currently available for download for free on for iOS on the App Store and for Android on the Google Play Store.',
+    info2:
+      'This app helps a child learn to read by showing them commonly used sight words and uses voice recognition to provide immediate feedback to let them know if they have read the word correctly.',
+    url: 'https://www.readingheroapp.com',
+    demoCta: 'More Info',
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +66,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'joshglazer@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,28 +74,33 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
+      name: 'github',
+      url: 'https://www.github.com/joshglazer',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/joshglazer/',
     },
     {
       id: nanoid(),
-      name: 'github',
-      url: '',
+      name: 'instagram',
+      url: 'https://www.instagram.com/glazerade/',
+    },
+    {
+      id: nanoid(),
+      name: 'facebook',
+      url: 'https://www.facebook.com/joshglazer82/',
+    },
+    {
+      id: nanoid(),
+      name: 'twitter',
+      url: 'https://twitter.com/glazerade',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
